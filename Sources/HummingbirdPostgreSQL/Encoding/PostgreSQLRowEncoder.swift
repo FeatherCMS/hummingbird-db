02fdde0edadddef3f08bfa1ca426c2543307465b
Tibor Bodecs <mail.tib@gmail.com>
2023-05-08 00:25:21 +0200

    implemented pg encoder

--- Sources/HummingbirdPostgreSQL/Encoding/PostgreSQLRowEncoder.swift
+++ Sources/HummingbirdPostgreSQL/Encoding/PostgreSQLRowEncoder.swift
@@ -1,27 +1,8 @@
 import PostgresNIO
 
+
 struct PostgreSQLRowEncoder {
-    var prefix: String? = nil
-    var keyEncodingStrategy: KeyEncodingStrategy = .useDefaultKeys
-    var nilEncodingStrategy: NilEncodingStrategy = .default
-
-    init() {}
-
-    func encode<E: Encodable>(
-        _ encodable: E
-    ) throws -> [(String, PostgresData)] {
-        let encoder = _Encoder(options: options)
-        try encodable.encode(to: encoder)
-        return encoder.row
-    }
-
-    enum NilEncodingStrategy {
-        /// Skips nilable columns with nil values during encoding.
-        case `default`
-        /// Encodes nilable columns with nil values as nil. Useful when using `SQLInsertBuilder` to insert `Codable` models without Fluent
-        case asNil
-    }
-
+    
     enum KeyEncodingStrategy {
         /// A key encoding strategy that doesn't change key names during encoding.
         case useDefaultKeys
@@ -30,246 +11,79 @@ struct PostgreSQLRowEncoder {
         case custom(([CodingKey]) -> CodingKey)
     }
 
-    fileprivate struct _Options {
+    struct _Options {
         let prefix: String?
         let keyEncodingStrategy: KeyEncodingStrategy
-        let nilEncodingStrategy: NilEncodingStrategy
     }
 
     /// The options set on the top-level decoder.
-    fileprivate var options: _Options {
+    var options: _Options {
         _Options(
             prefix: prefix,
-            keyEncodingStrategy: keyEncodingStrategy,
-            nilEncodingStrategy: nilEncodingStrategy
+            keyEncodingStrategy: keyEncodingStrategy
         )
+    }
+    
+    private var indexCounter = _IndexCounter()
+    var prefix: String? = nil
+    var keyEncodingStrategy: KeyEncodingStrategy = .useDefaultKeys
+
+    init() {}
+
+    func encode<E: Encodable>(
+        _ encodable: E
+    ) throws -> [(String, PostgresData)] {
+        let encoder = _Encoder(options: options, index: indexCounter.index)
+        try encodable.encode(to: encoder)
+        indexCounter.index += 1
+        return encoder.bindings
     }
 }
 
-private final class _Encoder: Encoder {
-    fileprivate let options: PostgreSQLRowEncoder._Options
+private final class _IndexCounter {
+    var index: Int = 0
+}
+
+private final class _Encoder: Encoder, SingleValueEncodingContainer {
+
+    let options: PostgreSQLRowEncoder._Options
+    let index: Int
 
     var codingPath: [CodingKey] { [] }
     var userInfo: [CodingUserInfoKey: Any] { [:] }
-    var row: [(String, PostgresData)]
 
-    init(options: PostgreSQLRowEncoder._Options) {
-        self.row = []
+    var bindings: [(String, PostgresData)]
+
+    init(options: PostgreSQLRowEncoder._Options, index: Int) {
+        self.bindings = []
         self.options = options
+        self.index = index
     }
 
     func container<Key: CodingKey>(keyedBy type: Key.Type)
         -> KeyedEncodingContainer<Key>
     {
-        switch options.nilEncodingStrategy {
-        case .asNil:
-            return KeyedEncodingContainer(_NilColumnKeyedEncoder(self))
-        case .default:
-            return KeyedEncodingContainer(_KeyedEncoder(self))
-        }
-    }
-
-    struct _NilColumnKeyedEncoder<Key: CodingKey>:
-        KeyedEncodingContainerProtocol
-    {
-        var codingPath: [CodingKey] { [] }
-        let encoder: _Encoder
-
-        init(_ encoder: _Encoder) {
-            self.encoder = encoder
-        }
-
-        func column(for key: Key) -> String {
-            var encodedKey = key.stringValue
-            switch encoder.options.keyEncodingStrategy {
-            case .useDefaultKeys:
-                break
-            case .convertToSnakeCase:
-                encodedKey = _convertToSnakeCase(encodedKey)
-            case .custom(let customKeyEncodingFunc):
-                encodedKey = customKeyEncodingFunc([key]).stringValue
-            }
-
-            if let prefix = encoder.options.prefix {
-                return prefix + encodedKey
-            }
-            return encodedKey
-        }
-
-        mutating func encodeNil(forKey key: Key) throws {
-            encoder.row.append((column(for: key), .null))
-        }
-
-        mutating func encode<T: Encodable>(_ value: T, forKey key: Key) throws {
-            let data = try PostgresDataEncoder().encode(value)
-            encoder.row.append((column(for: key), data))
-        }
-
-        mutating func _encodeIfPresent<T: Encodable>(
-            _ value: T?,
-            forKey key: Key
-        ) throws {
-            if let value = value {
-                try encode(value, forKey: key)
-            }
-            else {
-                try encodeNil(forKey: key)
-            }
-        }
-
-        mutating func encodeIfPresent<T: Encodable>(
-            _ value: T?,
-            forKey key: Key
-        ) throws {
-            try _encodeIfPresent(value, forKey: key)
-        }
-
-        mutating func encodeIfPresent(_ value: Int?, forKey key: Key) throws {
-            try _encodeIfPresent(value, forKey: key)
-        }
-
-        mutating func encodeIfPresent(_ value: Int8?, forKey key: Key) throws {
-            try _encodeIfPresent(value, forKey: key)
-        }
-
-        mutating func encodeIfPresent(_ value: Int16?, forKey key: Key) throws {
-            try _encodeIfPresent(value, forKey: key)
-        }
-
-        mutating func encodeIfPresent(_ value: Int32?, forKey key: Key) throws {
-            try _encodeIfPresent(value, forKey: key)
-        }
-
-        mutating func encodeIfPresent(_ value: Int64?, forKey key: Key) throws {
-            try _encodeIfPresent(value, forKey: key)
-        }
-
-        mutating func encodeIfPresent(_ value: UInt?, forKey key: Key) throws {
-            try _encodeIfPresent(value, forKey: key)
-        }
-
-        mutating func encodeIfPresent(_ value: UInt16?, forKey key: Key) throws
-        { try _encodeIfPresent(value, forKey: key) }
-
-        mutating func encodeIfPresent(_ value: UInt32?, forKey key: Key) throws
-        { try _encodeIfPresent(value, forKey: key) }
-
-        mutating func encodeIfPresent(_ value: UInt64?, forKey key: Key) throws
-        { try _encodeIfPresent(value, forKey: key) }
-
-        mutating func encodeIfPresent(_ value: Double?, forKey key: Key) throws
-        { try _encodeIfPresent(value, forKey: key) }
-
-        mutating func encodeIfPresent(_ value: Float?, forKey key: Key) throws {
-            try _encodeIfPresent(value, forKey: key)
-        }
-
-        mutating func encodeIfPresent(_ value: String?, forKey key: Key) throws
-        { try _encodeIfPresent(value, forKey: key) }
-
-        mutating func encodeIfPresent(_ value: Bool?, forKey key: Key) throws {
-            try _encodeIfPresent(value, forKey: key)
-        }
-
-        mutating func nestedContainer<NestedKey: CodingKey>(
-            keyedBy keyType: NestedKey.Type,
-            forKey key: Key
-        ) -> KeyedEncodingContainer<NestedKey> {
-            fatalError()
-        }
-
-        mutating func nestedUnkeyedContainer(forKey key: Key)
-            -> UnkeyedEncodingContainer
-        {
-            fatalError()
-        }
-
-        mutating func superEncoder() -> Encoder {
-            encoder
-        }
-
-        mutating func superEncoder(forKey key: Key) -> Encoder {
-            encoder
-        }
+        KeyedEncodingContainer(_KeyedEncoder(self))
     }
 
     func unkeyedContainer() -> UnkeyedEncodingContainer {
-        fatalError()
+        fatalError("Unkeyed container is not supported.")
     }
 
     func singleValueContainer() -> SingleValueEncodingContainer {
-        fatalError()
+        self
+    }
+    
+    func encodeNil() throws {
+        bindings.append((String(index), .null))
     }
 
-    struct _KeyedEncoder<Key: CodingKey>: KeyedEncodingContainerProtocol {
-
-        var codingPath: [CodingKey] { [] }
-        let encoder: _Encoder
-
-        init(_ encoder: _Encoder) {
-            self.encoder = encoder
-        }
-
-        func column(for key: Key) -> String {
-            var encodedKey = key.stringValue
-            switch encoder.options.keyEncodingStrategy {
-            case .useDefaultKeys:
-                break
-            case .convertToSnakeCase:
-                encodedKey = _convertToSnakeCase(encodedKey)
-            case .custom(let customKeyEncodingFunc):
-                encodedKey = customKeyEncodingFunc([key]).stringValue
-            }
-
-            if let prefix = encoder.options.prefix {
-                return prefix + encodedKey
-            }
-            return encodedKey
-        }
-
-        mutating func encodeNil(forKey key: Key) throws {
-            encoder.row.append((column(for: key), .null))
-        }
-
-        mutating func encode<T: Encodable>(_ value: T, forKey key: Key) throws {
-            let data = try PostgresDataEncoder().encode(value)
-            encoder.row.append((column(for: key), data))
-        }
-
-        mutating func nestedContainer<NestedKey: CodingKey>(
-            keyedBy keyType: NestedKey.Type,
-            forKey key: Key
-        ) -> KeyedEncodingContainer<NestedKey> {
-            fatalError()
-        }
-
-        mutating func nestedUnkeyedContainer(forKey key: Key)
-            -> UnkeyedEncodingContainer
-        {
-            fatalError()
-        }
-
-        mutating func superEncoder() -> Encoder {
-            encoder
-        }
-
-        mutating func superEncoder(forKey key: Key) -> Encoder {
-            encoder
-        }
-
-        func unkeyedContainer() -> UnkeyedEncodingContainer {
-            fatalError()
-        }
-
-        func singleValueContainer() -> SingleValueEncodingContainer {
-            fatalError()
-        }
+    func encode<T: Encodable>(_ value: T) throws {
+        let data = try PostgresDataEncoder().encode(value)
+        bindings.append((String(index), data))
     }
-}
 
-extension _Encoder {
-
-    fileprivate static func _convertToSnakeCase(_ stringKey: String) -> String {
+    func _convertToSnakeCase(_ stringKey: String) -> String {
         guard !stringKey.isEmpty else {
             return stringKey
         }
@@ -310,7 +124,72 @@ extension _Encoder {
 
             i = nextIndex
         }
-
         return snakeCasedString
+    }
+    
+}
+
+private struct _KeyedEncoder<Key: CodingKey>: KeyedEncodingContainerProtocol {
+
+    var codingPath: [CodingKey] { [] }
+    let encoder: _Encoder
+
+    init(_ encoder: _Encoder) {
+        self.encoder = encoder
+    }
+
+    func column(for key: Key) -> String {
+        var encodedKey = key.stringValue
+        switch encoder.options.keyEncodingStrategy {
+        case .useDefaultKeys:
+            break
+        case .convertToSnakeCase:
+            encodedKey = encoder._convertToSnakeCase(encodedKey)
+        case .custom(let customKeyEncodingFunc):
+            encodedKey = customKeyEncodingFunc([key]).stringValue
+        }
+
+        if let prefix = encoder.options.prefix {
+            return prefix + encodedKey
+        }
+        return encodedKey
+    }
+
+    mutating func encodeNil(forKey key: Key) throws {
+        encoder.bindings.append((column(for: key), .null))
+    }
+
+    mutating func encode<T: Encodable>(_ value: T, forKey key: Key) throws {
+        let data = try PostgresDataEncoder().encode(value)
+        encoder.bindings.append((column(for: key), data))
+    }
+
+    mutating func nestedContainer<NestedKey: CodingKey>(
+        keyedBy keyType: NestedKey.Type,
+        forKey key: Key
+    ) -> KeyedEncodingContainer<NestedKey> {
+        fatalError()
+    }
+
+    mutating func nestedUnkeyedContainer(forKey key: Key)
+        -> UnkeyedEncodingContainer
+    {
+        fatalError()
+    }
+
+    mutating func superEncoder() -> Encoder {
+        encoder
+    }
+
+    mutating func superEncoder(forKey key: Key) -> Encoder {
+        encoder
+    }
+
+    func unkeyedContainer() -> UnkeyedEncodingContainer {
+        fatalError()
+    }
+
+    func singleValueContainer() -> SingleValueEncodingContainer {
+        fatalError()
     }
 }
